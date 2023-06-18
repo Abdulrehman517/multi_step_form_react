b86db985edac9966e7a6075b3dc1304bc64d412d
Abdulrehman <72541146+Abdulrehman517@users.noreply.github.com>
2023-06-17 22:17:18 -0700

    content centerd

--- src/components/FormPersonalDetail.js
+++ src/components/FormPersonalDetail.js
@@ -17,7 +17,7 @@ export class FormPersonalDetail extends Component {
       <>
         <AppBar
           position="static"
-          style={{ display: "flex", alignItems: "center", height: "3rem" }}
+          style={{ display: "flex", alignItems: "center", height: "3rem" , justifyContent:"center"}}
         >
           Enter Personal Details
         </AppBar>
